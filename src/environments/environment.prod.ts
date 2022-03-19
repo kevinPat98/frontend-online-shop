81a68edc474219b96d265e4bc83f63317c84e983
Kevin Patiño <kevin.patino01@uptc.edu.co>
2022-03-19 01:41:22 -0500

    Envio a produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  backend: 'https://backend-meang.herokuapp.com/graphql',
+  backend: 'https://juegosboyaca.herokuapp.com/graphql',
+  backendWs: 'wss://juegosboyaca.herokuapp.com/graphql',
   stripePublicKey: 'pk_test_51KeV9lFZZFC4JuwgFYRMocR9y1imspHjtG2QoQnboiq42zwFmIYLZqmn4dNfMP3Q4yTwxlaDhxBcOyVQejVF0PSj00ScBGeWIY'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   backend: 'http://localhost:2002/graphql',
+  backendWs: 'ws://localhost:2002/graphql',
   stripePublicKey: 'pk_test_51KeV9lFZZFC4JuwgFYRMocR9y1imspHjtG2QoQnboiq42zwFmIYLZqmn4dNfMP3Q4yTwxlaDhxBcOyVQejVF0PSj00ScBGeWIY'
 };
 
